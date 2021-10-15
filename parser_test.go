78e05acc20636046602352265e95df3e777dfe30
Alexander Kabenin <alexander.kabenin@genesys.com>
2021-10-15 15:47:08 -0700

    Added ParseFile and ParseDir tests

--- parser_test.go
+++ parser_test.go
@@ -1,6 +1,7 @@
 package tfparser
 
 import (
+	"os"
 	"testing"
 )
 
@@ -105,5 +106,55 @@ func TestModule1Source(t *testing.T) {
 		if m.SourcePath != expSourcePath {
 			t.Fatalf("Unexpected source path for module 'legacy_use1_legacy_aps2_routing', found %#q, expected %#q", m.SourcePath, expSourcePath)
 		}
+	}
+}
+
+func TestParseFile(t *testing.T) {
+	testFileName := "testdata/tf/main.tf"
+	_, err := os.Stat(testFileName)
+	if os.IsNotExist(err) {
+		t.Skipf("testing terraform file is not found. Not testing ParseFile")
+	}
+	config, err := ParseFile(testFileName)
+	if err != nil {
+		t.Fatalf("ParseFile returned an error, %v", err)
+	}
+	assertTestingConfig1(config, t)
+}
+
+func TestParseDir(t *testing.T) {
+	testDirName := "testdata/tf/"
+	_, err := os.Stat(testDirName)
+	if os.IsNotExist(err) {
+		t.Skipf("testing terraform dir %#q is not found. Not testing ParseDir", testDirName)
+	}
+	config, err := ParseDir(testDirName)
+	if err != nil {
+		t.Fatalf("ParseDir returned an error, %v", err)
+	}
+	assertTestingConfig1(config, t)
+}
+
+func assertTestingConfig1(config *TFconfig, t *testing.T) {
+	if len(config.Modules) != 2 {
+		t.Fatalf("Unexpected number of moduels fetched. Expected 2 got %v", len(config.Modules))
+	}
+	m1, exists := config.Modules["module1"]
+	if !exists {
+		t.Fatalf("Module 'module1' is not found, but expected")
+	}
+	m1sp := "../../modules/module_name"
+	if m1.SourcePath != m1sp {
+		t.Fatalf("Unexpceted 'module1' source: %#q, expected %#q", m1.SourcePath, m1sp)
+	}
+	if len(m1.Providers) != 2 {
+		t.Fatalf("Unexpected number of providers for 'module1' %v, expected 2", len(m1.Providers))
+	}
+	p1, exists := m1.Providers["aws.alice"]
+	if !exists {
+		t.Fatal("provider 'aws.alice' was not found in 'module1")
+	}
+	if p1 != "aws.us-east-1" {
+		t.Fatalf("provider 'aws.alice' alias is %#q, expected 'aws.us-east-1'", p1)
 	}
 }
